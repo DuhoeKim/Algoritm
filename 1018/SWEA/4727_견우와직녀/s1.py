debb08a808e4b6b0a8d8f73667ab17fc7b801508
김두회 <style9604@naver.com>
2021-10-19 01:17:21 +0900

    4727. 견우와 직녀 수정

--- 1018/SWEA/4727_견우와직녀/s1.py
+++ 1018/SWEA/4727_견우와직녀/s1.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 import sys
 sys.stdin = open("input.txt")
 
@@ -7,8 +8,10 @@ for test_case in range(1, T+1):
 
     mat = [list(map(int, input().split())) for _ in range(N)]
 
-    shortcut = [[987654321]*N for _ in range(N)]
-    shortcut[0][0] = 0
+    shortcut = [[[987654321]*N for _ in range(N)] for _ in range(2)]
+
+    shortcut[0][0][0] = 0
+    shortcut[1][0][0] = 0
 
     Q = [[0, 0, 0, 0]]
 
@@ -23,7 +26,7 @@ for test_case in range(1, T+1):
             nc = c + dc[d]
 
             if 0 <= nr < N and 0 <= nc < N:
-                next_time = shortcut[nr][nc]
+                next_time = shortcut[is_used][nr][nc]
                 next_used = is_used
 
                 if mat[nr][nc]:
@@ -33,8 +36,8 @@ for test_case in range(1, T+1):
                     next_time = time + (M - time % M)
                     next_used = 1
 
-                if next_time < shortcut[nr][nc]:
-                    shortcut[nr][nc] = next_time
+                if next_time < shortcut[next_used][nr][nc]:
+                    shortcut[next_used][nr][nc] = next_time
                     Q.append([nr, nc, next_used, next_time])
 
-    print('#{} {}'.format(test_case, shortcut[-1][-1]))
+    print('#{} {}'.format(test_case, min(shortcut[0][-1][-1], shortcut[1][-1][-1])))
