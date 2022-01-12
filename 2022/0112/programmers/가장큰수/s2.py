74aa057bcbdde1fcb7a49ea9dfc399be19c9037a
Duhoe <style9604@naver.com>
2022-01-13 00:26:12 +0900

    프로그래머스 Lv2 한문제

--- 2022/0112/programmers/가장큰수/s2.py
+++ 2022/0112/programmers/가장큰수/s2.py
@@ -1,6 +1,19 @@
-def solution(numbers):
-    answer = ''
-    numbers.sort(key=lambda x: int(str(x)[0]) % 10, reverse=True)
-    
-
+# def solution(numbers):
+#     if not sum(numbers):
+#         return '0'
+#     numbers.sort(key=lambda x: (int(str(x)[0]) % 10, int(str(x)[1 if len(str(x)) >= 2 else 0]) % 10, int(str(x)[2 if len(str(x)) >= 3 else 0]) % 10, int(str(x)[3 if len(str(x)) >= 4 else 0]) % 10), reverse=True)
+#     print(numbers)
+#     answer = ''.join(map(str, numbers))
+#     return answer
+def s2(numbers):
+    if not sum(numbers):
+        return '0'
+    numbers.sort(key=lambda x: int((str(x)*4)[:4]), reverse=True)
+    print(numbers)
+    answer = ''.join(map(str, numbers))
     return answer
+arr = [90, 901, 9097]
+# a = int(solution(arr))
+b = int(s2(arr))
+
+# print(a-b, b-a)
